6d8f782cd7b399c4ea91d7bca641833b3dfe3dc7
Penguinizer <anttimatik@gmail.com>
2020-03-31 14:43:26 +0300

    Added main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+	[SerializeField]
+	bool sakuSandbox;
+	[SerializeField]
+	bool background;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material.color=Color.black;
+    }
+
+	void OnMouseEnter(){
+		GetComponent<Renderer>().material.color = Color.red;
+	}
+	
+	void OnMouseExit(){
+		GetComponent<Renderer>().material.color=Color.black;
+	}
+     
+	 void OnMouseUp(){
+		 if (sakuSandbox){
+			 Application.LoadLevel("Saku_Sandbox");
+		 }
+		 if (background){
+			 Application.LoadLevel("Background");
+		 }
+	 }
+}
