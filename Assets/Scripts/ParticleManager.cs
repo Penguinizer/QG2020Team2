70f60cccd6e716d62fef7da0840d33a9fd888827
sakulainen <anttila.saku@gmail.com>
2020-04-06 13:57:02 +0300

    particle effuja

---
+++ Assets/Scripts/ParticleManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ParticleManager : MonoBehaviour
+{
+
+   [SerializeField] GameObject annihilationParticles;
+    public bool ffToggle = true;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (gameObject.tag == "MinusBall" && collision.collider.tag == "PosBall" && (gameObject.transform.parent.tag != collision.collider.transform.parent.tag | ffToggle))
+        {
+            print("Play annihilation particles");
+            Instantiate(annihilationParticles, transform.position, transform.rotation);
+
+
+        }
+        if (gameObject.tag == "PosBall" && collision.collider.tag == "MinusBall" && (gameObject.transform.parent.tag != collision.collider.transform.parent.tag | ffToggle))
+        {
+
+            print("Play annihilation particles");
+            
+        }
+        if (collision.collider.tag == "Hole")
+        {
+
+        }
+
+
+
+
+    }
+}
