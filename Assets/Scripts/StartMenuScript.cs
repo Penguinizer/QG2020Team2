8a3dd86c24ae618e40751d9f20cf6361925de3d2
Penguinizer <antti.o.matikainen@aalto.fi>
2020-04-24 16:33:33 +0300

    Updated tutorial text

--- Assets/Scripts/StartMenuScript.cs
+++ Assets/Scripts/StartMenuScript.cs
@@ -22,13 +22,13 @@ public class StartMenuScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-		content = new GUIContent("Player 1 Controls:\n Movement: Arrow Keys.\n Impulse: Numpad 1. Minus Particle Numpad 2. Positive Particle: Numpad 3.\n Area Control Particle: Numpad 0. \n\nPlayer 2 Controls:\n Movement: WASD. Impulse: Z. Minus Particle: X. Positive Particle: X.\n Area Control Particle V.");
+		content = new GUIContent("Player 1 Controls:\n Movement: Arrow Keys.\n Impulse: Numpad 1. Minus Particle Numpad 2. Positive Particle: Numpad 3.\n Area Control Particle: Numpad 0. \n\nPlayer 2 Controls:\n Movement: WASD. Impulse: Z. Minus Particle: X. Positive Particle: X.\n Area Control Particle V.\n The goal of the game is to gather photons created by annihilating particles to gain energy.\n This energy is used to create area control particles. Create enough area control particles to win.");
     }
 	
 	void OnGUI(){
 		GUI.Box(new Rect((Screen.width/2)-300, (Screen.height/2)-300, 600, 600), content);
 		
-		if(GUI.Button(new Rect((Screen.width/2)-100, (Screen.height/2)+100, 200, 100), "Press To Start Game")){
+		if(GUI.Button(new Rect((Screen.width/2)-100, (Screen.height/2)+150, 200, 100), "Press To Start Game")){
 			unpausePlayers();
 			Destroy(gameObject);
 		}			
