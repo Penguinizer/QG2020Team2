2ca733a061d2b23c33af101ce8ec747536c029cc
Penguinizer <antti.o.matikainen@aalto.fi>
2020-04-27 21:01:46 +0300

    Added OOB block triggers.

---
+++ Assets/Scripts/OOBTrigger.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OOBTrigger : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col){
+		col.transform.position = new Vector3 (0,0,0);
+	}
+}
